c82aaf2577a45974458924b3f98416018f63b34d
ogimogi98 <ogulcanyildirim98@gmail.com>
2022-06-16 12:55:47 +0300

    Version 2.1 : -Studenten Hinzufügen,Auflisten erstellt.

--- MainDemo/src/main/java/com/company/maindemo/adminStudentPageController.java
+++ MainDemo/src/main/java/com/company/maindemo/adminStudentPageController.java
@@ -1,15 +1,22 @@
 package com.company.maindemo;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
+import javax.security.auth.callback.Callback;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.*;
 
 public class adminStudentPageController {
 
@@ -55,6 +62,7 @@ public class adminStudentPageController {
     @FXML
     AnchorPane hinzufugenAnchorPane;
     @FXML SplitPane auflistenSplitPane;
+    @FXML ObservableList<ObservableList> data;
 
     Alert a = new Alert(Alert.AlertType.CONFIRMATION);
     ObservableList<String> geschlectArten = FXCollections.observableArrayList("Mannlich","Weiblich","Diversity");
@@ -160,7 +168,46 @@ public class adminStudentPageController {
                 String query = "Select studentID, TC, Name, Nachname, from student"+geschlect+sort;
                 System.out.println(query);
                 PreparedStatement preparedStatement = prMethods.connection.prepareStatement(query);
-                ResultSet resultSet = preparedStatement.executeQuery();
+                ResultSet rs = preparedStatement.executeQuery();
+
+
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+
+                studentTableview.getColumns().addAll(col);
+                System.out.println("Column ["+i+"] ");
+            }
+
+            /********************************
+             * Data added to ObservableList *
+             ********************************/
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            studentTableview.setItems(data);
+        }catch(Exception e){
+        e.printStackTrace();
+        System.out.println("Error on Building Data");
+    }
 
         }
 
